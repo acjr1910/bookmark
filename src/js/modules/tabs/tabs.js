215afb078b924edba936224de0a47f0a1cc87249
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-06-05 15:57:28 -0300

    tabs module is done

--- src/js/modules/tabs/tabs.js
+++ src/js/modules/tabs/tabs.js
@@ -3,16 +3,49 @@ const tabs = (function menuBehavior() {
 
   const tabList = document.querySelectorAll('.tabs__item');
 
+  // @param { arrList } <NodeList>;
+  // @param { classname } <string>;
+  function removeTabsClass(arrList, classname) {
+    if (!Array.isArray(Array.from(arrList)) || typeof classname !== 'string')
+      return null;
+    return arrList.forEach((elem) => elem.classList.remove(classname));
+  }
+
+  // @param { tab } <HtmlElement>;
+  function addSelectedTabClass(tab) {
+    return tab.classList.add('tabs__item--active');
+  }
+
+  // @param { tab } <HtmlElement>;
+  function handleActiveClass(tab) {
+    removeTabsClass(tabList, 'tabs__item--active');
+    addSelectedTabClass(tab);
+  }
+
+  // @param { tab } <HtmlElement>;
+  function handleActiveContent(tab) {
+    // Current tab data
+    const selectedImageSource = tab.getAttribute('data-image-src');
+    const selectedHeading = tab.getAttribute('data-heading');
+    const selectedParagraph = tab.getAttribute('data-paragraph');
+
+    // Selected tab data placeholder's
+    const imageToShow = document.querySelector('.tab-content img');
+    const headingToShow = document.querySelector('.tab-content h2');
+    const paragraphToShow = document.querySelector('.tab-content p');
+
+    // Set selected tab content
+    imageToShow.setAttribute('src', selectedImageSource);
+    headingToShow.innerHTML = selectedHeading;
+    paragraphToShow.innerHTML = selectedParagraph;
+  }
+
+  // @param { tab } <HtmlElement>;
   function handleTabClick(tab) {
     if (!tab) return null;
-    console.log();
-    const imageSource = tab.getAttribute('data-image-src');
-    const heading = tab.getAttribute('data-heading');
-    const paragraph = tab.getAttribute('data-paragraph');
-    console.log(imageSource, heading, paragraph);
-
-    const imageToShow = document.querySelector('.tab-content img');
-    imageToShow.setAttribute('src', imageSource);
+    handleActiveClass(tab);
+    handleActiveContent(tab);
+    return;
   }
 
   function addClickListener() {
