02ada01135fbb9f444640b9bba2c7ab1fe161431
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-06-15 10:02:20 -0300

    created tabs tests

---
+++ src/js/modules/tabs/tabs.spec.js
@@ -0,0 +1,13 @@
+import tabs from './tabs';
+
+describe('tabs.js', function() {
+  it('should remove class from not clicked element', function() {
+    
+  });
+  it('should add class to the cliked element', function() {
+
+  });
+  it('must have only one element with class tabs__item--active', function() {
+
+  });
+});
