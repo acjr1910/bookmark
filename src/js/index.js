043df086490782fc12f67ee89350b787d54ebbda
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-06-22 20:28:25 -0300

    added collapse behavior

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,12 @@ require('../images/illustration-features-tab-3.svg');
 
 import menu from './modules/menu/menu';
 import tabs from './modules/tabs/tabs';
+import collapse from './modules/collapse/collapse';
 
 import styles from '../styles/index.scss';
 
 window.addEventListener('DOMContentLoaded', function (e) {
   menu.init();
   tabs.init();
+  collapse.applyBehavior();
 });
---
+++ src/js/modules/collapse/collapse.js
@@ -0,0 +1,22 @@
+const collapse = (function () {
+  const showClass = 'collapse__item--show';
+  const collapseItems = Array.from(
+    document.getElementsByClassName('collapse__item')
+  );
+
+  function toggleClass(item) {
+    item.classList.toggle(showClass);
+    console.log(item.classList);
+  }
+
+  function applyBehavior() {
+    collapseItems.map((item) => {
+      item.addEventListener('click', () => toggleClass(item));
+    });
+  }
+  return {
+    applyBehavior,
+  };
+})();
+
+export default collapse;
