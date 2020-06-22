5e0c62baa1d67645f9f36bf72b3a7b1f3324009e
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-06-22 18:25:50 -0300

    added constants for tabs testing and implemented tabs testing

---
+++ src/js/modules/tabs/constants/index.js
@@ -0,0 +1,32 @@
+export const HTML = `<ul class="tabs">
+<li class="tabs__item tabs__item--active" data-image-src="images/illustration-features-tab-1.svg" data-heading="Bookmark in one click" data-paragraph="Organize your bookmarks however you like. Our simple drag-and-drop
+interface gives you complete control over how you manage your
+favourite sites.">
+  Simple Bookmarking
+</li>
+<li class="tabs__item" data-image-src="images/illustration-features-tab-2.svg" data-heading="Intelligent search" data-paragraph="Our powerful search feature will help you find saved sites in no time at all. 
+  No need to trawl through all of your bookmarks.">
+  Speedy Searching
+</li>
+<li class="tabs__item" data-image-src="images/illustration-features-tab-3.svg" data-heading=" Share your bookmarks" data-paragraph="Easily share your bookmarks and collections with others. Create a shareable 
+  link that you can send at the click of a button.">
+  Easy Sharing
+</li>
+</ul>
+<div class="l-grid-column tab-content">
+<div class="background-left">
+<img
+  class="tab-content__image"
+  src="images/illustration-features-tab-1.svg"
+  alt="Selected Tab Image"
+/>
+</div>
+<div class="container">
+<h2>Bookmark in one click</h2>
+<p>
+  Organize your bookmarks however you like. Our simple drag-and-drop
+  interface gives you complete control over how you manage your
+  favourite sites.
+</p>
+</div>
+</div>`;
--- src/js/modules/tabs/tabs.spec.js
+++ src/js/modules/tabs/tabs.spec.js
@@ -1,25 +1,35 @@
+import { HTML } from './constants/index';
+
 describe('tabs.js', function () {
-  document &&
-    (document.body.innerHTML = `<ul class="tabs">
-            <li class="tabs__item tabs__item--active" data-image-src="images/illustration-features-tab-1.svg" data-heading="Bookmark in one click" data-paragraph="Organize your bookmarks however you like. Our simple drag-and-drop
-          interface gives you complete control over how you manage your
-          favourite sites.">
-              Simple Bookmarking
-            </li>
-            <li class="tabs__item" data-image-src="images/illustration-features-tab-2.svg" data-heading="Intelligent search" data-paragraph="Our powerful search feature will help you find saved sites in no time at all. 
-              No need to trawl through all of your bookmarks.">
-              Speedy Searching
-            </li>
-            <li class="tabs__item" data-image-src="images/illustration-features-tab-3.svg" data-heading=" Share your bookmarks" data-paragraph="Easily share your bookmarks and collections with others. Create a shareable 
-              link that you can send at the click of a button.">
-              Easy Sharing
-            </li>
-          </ul>`);
+  document && (document.body.innerHTML = HTML);
 
   const tabs = require('./tabs');
   tabs.default.init();
 
-  it('should remove class from not clicked element', function () {});
-  it('should add class to the cliked element', function () {});
-  it('must have only one element with class tabs__item--active', function () {});
+  it('should add class to the cliked element', function () {
+    const tabList = document.getElementsByClassName('tabs__item');
+    const [firstTab, secondTab, thirdTab] = tabList;
+
+    firstTab.click();
+    expect(firstTab.classList.contains('tabs__item--active')).toBeTruthy();
+
+    secondTab.click();
+    expect(secondTab.classList.contains('tabs__item--active')).toBeTruthy();
+
+    thirdTab.click();
+    expect(thirdTab.classList.contains('tabs__item--active')).toBeTruthy();
+  });
+
+  it('must have only one element with class tabs__item--active', function () {
+    const tabList = Array.from(document.getElementsByClassName('tabs__item'));
+    expect(tabList).toBeTruthy();
+
+    tabList.map((tab) => {
+      tab.click();
+      const activeTabsLength = document.getElementsByClassName(
+        'tabs__item--active'
+      ).length;
+      expect(activeTabsLength).toBe(1);
+    });
+  });
 });
